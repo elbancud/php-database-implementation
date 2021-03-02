59589e139b89b88a2edb8c877f5f495abbd8323c
elbancud <elbancud@gmail.com>
2021-03-02 22:52:12 +0800

    container fixed

--- index.php
+++ index.php
@@ -18,41 +18,42 @@ include_once 'db.php';
 <body>
     <div class="container">
         <form action="" method="GET">
-            <input type="text" name="name" placeholder="Name" autocomplete="off">
-            <input type="text" name="lastname" placeholder="Lastname" autocomplete="off">
+
+
             <input type="text" name="username" placeholder="Username" autocomplete="off">
             <input type="text" name="password" placeholder="Passwrod" autocomplete="off">
             <br />
             <button type="submit" name="register">Register</button>
+
+            <?php
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            if (isset($_GET['register'])) {
+                ($_GET['username'] == '' || $_GET['password'] == '') {
+                    print("Please fill the fields");
+                } else {
+                    print("Good to go tanga");
+
+                    $username = $_GET['username'];
+                    $password = $_GET['password'];
+
+
+                    $sql = "INSERT INTO adminprofile (username, password)
+                    VALUES ('$username', '$password')";
+
+                    if ($conn->query($sql) === TRUE) {
+                        echo "New record created successfully";
+                    } else {
+                        echo "Error: " . $sql . "<br>" . $conn->error;
+                    }
+                }
+            }
+            ?>
         </form>
     </div>
 
-    <?php
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    if (isset($_GET['register'])) {
-        if ($_GET['username'] == '' || $_GET['password'] == '') {
-            print("Please enter");
-        } else {
-            print("Good to go tanga");
-
-            $username = $_GET['username'];
-            $password = $_GET['password'];
-
-
-            $sql = "INSERT INTO adminprofile (username, password)
-                    VALUES ('$username', '$password')";
-
-            if ($conn->query($sql) === TRUE) {
-                echo "New record created successfully";
-            } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-        }
-    }
-    ?>
 </body>
 
 </html>
